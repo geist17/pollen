f54c17cac0775a1c1b99136139feee92166c88e2
MDAVQJDSKQ <spencerhodgemusic@gmail.com>
2024-09-18 21:41:31 -0400

    Updates

---
+++ _next/static/Ise6UaMIS9VacNyou6Y4Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/de14cc210266d2e0.css","static/chunks/pages/index-3b21089cbcf5c3bf.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
